20370637cf3be7194a099d635a9c57a5650557d7
Bruce Ediger <bediger8@gmail.com>
2024-01-29 07:14:12 -0700

    count valid heaps as per problem statement

---
+++ heapcount.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"heapcounter/heap"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var ary []int
+	for _, str := range os.Args[1:] {
+		if n, err := strconv.Atoi(str); err == nil {
+			ary = append(ary, n)
+		}
+	}
+
+	c := make(chan []int)
+
+	go outerGenerate(c, ary)
+
+	count := 0
+	for h := range c {
+		hp := heap.Heap(h)
+		if hp.IsHeap() {
+			count++
+			fmt.Printf("%s\t%v\n", hp.ToString(), h)
+		}
+	}
+	fmt.Printf("%d heaps possible\n", count)
+}
+
+func outerGenerate(c chan []int, ary []int) {
+	generate(c, len(ary), ary)
+	close(c)
+}
+
+func generate(c chan []int, k int, a []int) {
+	if k == 1 {
+		cpy := make([]int, len(a))
+		copy(cpy, a)
+		c <- cpy
+		return
+	}
+
+	generate(c, k-1, a)
+
+	for i := 0; i < k-1; i++ {
+		if (k % 2) == 0 {
+			a[i], a[k-1] = a[k-1], a[i]
+		} else {
+			a[0], a[k-1] = a[k-1], a[0]
+		}
+		generate(c, k-1, a)
+	}
+}
